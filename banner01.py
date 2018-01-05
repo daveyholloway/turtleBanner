c56ab481453601cdb5b736c86c55d0e165a41fdd
Dave <davey.holloway@gmail.com>
2018-01-05 10:58:28 +0000

    Add files via upload
    Added "C"!

--- banner01.py
+++ banner01.py
@@ -17,17 +17,25 @@ import turtle
 def getSequence(pChar):
 
     if pChar == "A":
-        return ["U","F1","S1", "D","F2","L90","F2","R90","F2","R90","F2","L90","F2", \
-                "L90","F6","L90","F1","R90","F1","L90","F4","L90","F1","R90", \
-                "F1","L90","F6","L90", "E","U","F2","L90","F3","D","S2", "F3","R90","F2", \
-                "R90","F3","R90","F2","E", "U","L180"]
+        return ["U","F1","L90", "F1", "R90","S1", "D","F2","L90","F2","R90", \
+                "F2","R90","F2","L90","F2", "L90","F6","L90","F1","R90","F1", \
+                "L90","F4","L90","F1","R90", "F1","L90","F6","L90", "E","U", \
+                "F2","L90","F3","D","S2", "F3","R90","F2", "R90","F3","R90", \
+                "F2","E", "U","L180"]
     elif pChar == "B":
-        return ["U","F1","D","S1", "F5","L90","F1","R90","F1","L90","F2","L90","F1", \
-                "R90","F1","R90","F1","L90","F2","L90","F1","R90","F1","L90", \
-                "F5","L90","F7","L90","E", "U","F2","L90","F1","D","S2", "F2","R90","F2", \
-                "R90","F2","R90","F2","E", "U","R90","F3","D","S2","F2","R90","F2","R90", \
+        return ["U","F1","L90", "F1", "R90","D","S1", "F5","L90","F1","R90", \
+                "F1","L90","F2","L90","F1", "R90","F1","R90","F1","L90","F2", \
+                "L90","F1","R90","F1","L90", "F5","L90","F7","L90","E", "U", \
+                "F2","L90","F1","D","S2", "F2","R90","F2", "R90","F2","R90", \
+                "F2","E", "U","R90","F3","D","S2","F2","R90","F2","R90", \
                 "F2","R90","F2","E", "U","L180"]
-
+    elif pChar == "C":
+        return ["U","F1","L90", "F1", "R90", "F1", "D","S1", "F4","L90","F1", \
+                "R90", "F1","L90","F1","L90", "F2", "L90","F1","R90","F2", \
+                "R90","F5", "R90", "F2", "R90", "F1", "L90", "F2", "L90", \
+                "F1", "L90", "F1", "R90", "F1", "L90", "F4", "L90", "F1", \
+                "R90", "F1", "L90", "F5", "L90", "F1", "R90", "F1","E", \
+                "U", "L90"]
     else:
         return ["D","F8","L90","F8","L90","F8","L90","F8","L90"]
 
@@ -55,27 +63,26 @@ def plotSequence(pTurtle, pX, pY, pSize, pLine, pFill1, pFill2, pSequence):
 
         # Determine the instruction
         wInstruction = pSequence[i][0]
-        
+
         if wInstruction == "F":          # forward
             pTurtle.forward(int(pSequence[i][1:len(pSequence[i])]) * pSize)
-            
+
         elif wInstruction == "B":        # backwards
             pTurtle.backwards(int(pSequence[i][1:len(pSequence[i])]) * pSize)
-            
+
         elif wInstruction == "L":        # left
             pTurtle.left(int(pSequence[i][1:len(pSequence[i])]))
-            
+
         elif wInstruction == "R":        # right
             pTurtle.right(int(pSequence[i][1:len(pSequence[i])]))
-            
+
         elif wInstruction == "U":        # pen up
             pTurtle.penup()
-            
+
         elif wInstruction == "D":        # pen down
             pTurtle.pendown()
 
         elif wInstruction == "S":        # begin fill
-
             # The number after S determines which of the 2 fill colours to use
             if pSequence[i][1:len(pSequence[i])] == "1":
                 pTurtle.color(pLine,pFill1)
@@ -83,7 +90,7 @@ def plotSequence(pTurtle, pX, pY, pSize, pLine, pFill1, pFill2, pSequence):
                 pTurtle.color(pLine,pFill2)
 
             pTurtle.begin_fill()
-            
+
         elif wInstruction == "E":        # end fill
             pTurtle.end_fill()
 
@@ -94,8 +101,8 @@ def plotSequence(pTurtle, pX, pY, pSize, pLine, pFill1, pFill2, pSequence):
 ################################################################################
 
 bob=turtle.Turtle()
-bob.left(20)
 
+plotSequence(bob, -50, 0, 5, "Black", "Green", "White", getSequence("C"))
 plotSequence(bob, 0, 0, 5, "Black", "Yellow", "White", getSequence("A"))
-plotSequence(bob, 50, 0, 7, "Black", "Red", "White", getSequence("B"))
-plotSequence(bob, 150, 0, 9, "Black", "Yellow", "White", getSequence("C"))
+plotSequence(bob, 50, 0, 5, "Black", "Red", "White", getSequence("B"))
+plotSequence(bob, 100, 0, 5, "Black", "Blue", "White", getSequence("C"))
